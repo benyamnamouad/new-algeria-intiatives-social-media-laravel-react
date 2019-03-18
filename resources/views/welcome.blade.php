ff0f9f268b5e3906b1b7431ce49af6846ce61b61
Mouad Benyamna <dev_bnmouad@hotmail.com>
2019-03-19 00:33:36 +0100

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="/css/app.css" type="text/css">
+
+
+    </head>
+    <body>
+       <div id="example"> </div>
+
+       <script src="/js/app.js"> </script>
+    </body>
+</html>
